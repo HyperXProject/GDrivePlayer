d10337cb25c66a1f73f5e9aa995c9105867248a0
HyperXProject <HyperXProject@users.noreply.github.com>
2017-08-30 11:40:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+$username = "admin";
+$password = "0145440762";
+$nonsense = "supercalifragilisticexpialidocious";
+
+if (isset($_COOKIE['PrivatePageLogin'])) {
+   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+?>
+
+<?php
 	error_reporting(0);
 	include "curl_gd.php";
 
@@ -55,3 +64,33 @@
 
 </body>
 </html>
+
+
+<?php
+      exit;
+   } else {
+      echo "Bad Cookie.";
+      exit;
+   }
+}
+
+if (isset($_GET['p']) && $_GET['p'] == "login") {
+   if ($_POST['user'] != $username) {
+      echo "Sorry, that username does not match.";
+      exit;
+   } else if ($_POST['keypass'] != $password) {
+      echo "Sorry, that password does not match.";
+      exit;
+   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+      echo "Sorry, you could not be logged in at this time.";
+   }
+}
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+<label><input type="text" name="user" id="user" /> UserName</label><br />
+<label><input type="password" name="keypass" id="keypass" /> Password</label><br />
+<input type="submit" id="submit" value="Login" />
+</form>
